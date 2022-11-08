9f7ed31447ef37407f0d8481aa7f292d55f0331a
masato <m.ishikawa.sys22@morijyobi.ac.jp>
2022-11-08 10:34:55 +0900

    ñlª̌vZðüÍ

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 public class Main{
     public static void main(String[] args) {
-        double weight=80;
-        double height=1.82;
-        double bmi=weight/(height*height);
-        System.out.println(String.format("%.2f", bmi));
+        double weight1=80;
+        double height1=1.82;
+        double bmi1=weight1/(height1*height1);
+        System.out.println(String.format("%.2f", bmi1));
+        double weight2=62;
+        double height2=1.69;
+        double bmi2=weight2/(height2*height2);
+        System.out.println(String.format("%.2f", bmi2));
     }
 }
